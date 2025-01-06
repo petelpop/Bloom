dfc285ea5df5341397b236b391c58ef579d0217e
naufal syuja <m.naufal.s.k@gmail.com>
2025-01-06 13:31:00 +0700

    create splash page

---
+++ lib/feature/splash/presentation/views/splash_page.dart
@@ -0,0 +1,52 @@
+import 'package:bloom/common/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+class SplashPage extends StatefulWidget {
+  static const String routeName = "splash-page";
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+
+
+class _SplashPageState extends State<SplashPage> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(Duration(seconds: 2), () {
+      
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  Constants.bgSplashScreen
+                ),
+                fit: BoxFit.cover)
+            ),
+          ),
+          Center(
+            child: Image.asset(
+              Constants.icTextBloomWhite,
+              width: 50.w,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
