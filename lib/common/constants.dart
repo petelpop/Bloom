9076bdf6263020627338851ee968b83f9a9d4a8e
naufal syuja <m.naufal.s.k@gmail.com>
2025-01-06 19:58:58 +0700

    change using fonts method

---
+++ lib/common/constants.dart
@@ -0,0 +1,10 @@
+class Constants {
+  static const String iconsPath = "assets/icons";
+  static const String icTextBloomWhite = "$iconsPath/ic_text_bloom_white.png";
+
+  static const String imagePath = "assets/image";
+  static const String bgSplashScreen = "$imagePath/bg_splash_screen.png";
+  static const String onBoardingFirst = "$imagePath/onboarding_first.png";
+  static const String onBoardingSecond = "$imagePath/onboarding_second.png";
+  static const String onBoardingThird = "$imagePath/onboarding_third.png";
+}
