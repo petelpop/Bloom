7d64c3e672710c503b2b52931eed3473b5dbb061
naufal syuja <m.naufal.s.k@gmail.com>
2025-01-08 23:47:41 +0700

    fixing route

--- lib/utils/route.dart
+++ lib/utils/route.dart
@@ -10,35 +10,36 @@ import 'package:bloom/feature/splash/presentation/views/splash_page.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRoute {
-  static final GoRouter router = GoRouter(
-    initialLocation: '/${SplashPage.routeName}',
-    routes: [
-      GoRoute(
-        path: '/${SplashPage.routeName}',
-        name: SplashPage.routeName,
+  static final GoRouter router =
+      GoRouter(initialLocation: '/${SplashPage.routeName}', routes: [
+    GoRoute(
+      path: '/${SplashPage.routeName}',
+      name: SplashPage.routeName,
+      builder: (context, state) {
+        return const SplashPage();
+      },
+    ),
+    GoRoute(
+        path: '/${OnboardingPage.routeName}',
+        name: OnboardingPage.routeName,
         builder: (context, state) {
-          return const SplashPage();
-        },),
-        GoRoute(
-          path: '/${OnboardingPage.routeName}',
-          name: OnboardingPage.routeName,
-          builder:(context, state) {
-            return OnboardingPage();
-          }),
-          GoRoute(
-            path: '/${MainPage.routeName}',
-            name: MainPage.routeName,
-            builder: (context, state) {
-              return const MainPage();
-            },),
-          GoRoute(
-            path: '/${HomePage.routeName}',
-            name: HomePage.routeName,
-            builder: (context, state) {
-              return HomePage();
-            }),
+          return OnboardingPage();
+        }),
+    GoRoute(
+      path: '/${MainPage.routeName}',
+      name: MainPage.routeName,
+      builder: (context, state) {
+        return const MainPage();
+      },
+      routes: [
             GoRoute(
-              path: '/${PilahPage.routeName}',
+        path: HomePage.routeName,
+        name: HomePage.routeName,
+        builder: (context, state) {
+          return HomePage();
+        },),
+                  GoRoute(
+              path: PilahPage.routeName,
               name: PilahPage.routeName,
               builder: (context, state) {
                 return PilahPage();
@@ -50,23 +51,26 @@ class AppRoute {
                   builder: (context, state) {
                     return ResultPage(
                       pilah: state.extra as PilahModel,
-                      );
-                  },)
-              ]
-            ),
+                    );
+                  },
+                )
+              ]),
           GoRoute(
-            path: '/${FloraPage.routeName}',
-            name: FloraPage.routeName,
-            builder: (context, state) {
-              return FloraPage();
-            },
-            routes: [
-        GoRoute(
-          path: ChatbotPage.routeName,
-          name: ChatbotPage.routeName,
-          builder: (context, state) {
-            return ChatbotPage();
-          },),
-            ])
-    ]);
+              path: FloraPage.routeName,
+              name: FloraPage.routeName,
+              builder: (context, state) {
+                return FloraPage();
+              },
+              routes: [
+                GoRoute(
+                  path: ChatbotPage.routeName,
+                  name: ChatbotPage.routeName,
+                  builder: (context, state) {
+                    return ChatbotPage();
+                  },
+                ),
+              ])
+      ]
+    ),
+  ]);
 }
