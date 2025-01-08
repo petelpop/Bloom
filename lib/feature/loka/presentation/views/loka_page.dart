ab7d832e3cb4dfbaea9c7b513274d345c99c5807
naufal syuja <m.naufal.s.k@gmail.com>
2025-01-08 20:02:04 +0700

    create loka, pilah & article page for fill the listPage

---
+++ lib/feature/loka/presentation/views/loka_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LokaPage extends StatelessWidget {
+  const LokaPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
